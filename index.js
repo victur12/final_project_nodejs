a95457468dec4c1eace1b87a8f8debba21b7a163
Victor González <vgonzalezvictor0@gmail.com>
2022-12-10 23:42:49 -0600

    Install dependencies and configure db

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const morgan = require('morgan');
+const employees = require('./routes/employees');
+const user = require('./routes/user');
+//middleware
+const auth = require('./middleware/auth');
+const notFound = require('./middleware/notFound');
+const cors = require('./middleware/cors');
 
-app.get('/', (req, res, next) => {
-    res.send('Hello World!')
+app.use(cors);
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+app.get("/", (req,res, next)=>{
+    res.status(200).json({code: 1, message: "Bienveniedo al pokedex"});
+    res.send("hola");
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}!`)
+app.use("/user", user);
+//autotentificacion
+app.use(auth);
+app.use("/employees", employees);
+
+app.use(notFound);
+
+
+app.listen(process.env.PORT || port, ()=>{
+    console.log('server is running...');
 });
 
